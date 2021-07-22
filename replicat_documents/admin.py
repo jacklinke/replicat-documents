4d496a649fa990b42fdf11e760ffccd6a412362f
Jack Linke <jacklinke@gmail.com>
2021-07-22 00:28:04 -0400

    💄(admin) Improve display for ReplicatDocument admin

--- replicat_documents/admin.py
+++ replicat_documents/admin.py
@@ -5,10 +5,30 @@ from .models import ReplicatDocument
 
 @admin.register(ReplicatDocument)
 class ReplicatDocumentAdmin(admin.ModelAdmin):
-    list_display = [
+    list_display = (
         "id",
         "document_id",
         "created_at",
         "updated_at",
         "rendered_to_pdf_at",
-    ]
+    )
+
+    list_filter = ("issuer",)
+    readonly_fields = (
+        "id",
+        "document_id",
+        "context",
+        "rendered_to_pdf_at",
+        "created_at",
+        "updated_at",
+    )
+
+    fieldsets = (
+        (None, {"fields": ("id", "document_id", "issuer", "context", "context_query")}),
+        (
+            "Dates",
+            {
+                "fields": ("rendered_to_pdf_at", "created_at", "updated_at"),
+            },
+        ),
+    )
